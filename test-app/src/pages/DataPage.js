ff291c0ba3f51d128068aa0d7086a3a43a02b89f
ritamxy <58613546+ritamxy@users.noreply.github.com>
2021-11-07 19:49:29 +1100

    create table draft for data page

---
+++ test-app/src/pages/DataPage.js
@@ -0,0 +1,19 @@
+function DataPage() {
+  return (
+    <div>
+      <h1>Tracking Data from GA</h1>
+      <table>
+        <caption>Audience Overview</caption>
+        <tr>
+          <th>Sessions</th>
+          <th>Users</th>
+          <th>Pageviews</th>
+          <th>Bounce Rate</th>
+          <th>New Sessions</th>
+        </tr>
+      </table>
+    </div>
+  );
+}
+
+export default DataPage;
