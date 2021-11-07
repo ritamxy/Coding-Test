3a8d71525f2491d84f1c8ec3e4463cd12a9255af
ritamxy <58613546+ritamxy@users.noreply.github.com>
2021-11-07 19:54:00 +1100

    link DataPage to Login button

--- test-app/src/pages/LoginPage.js
+++ test-app/src/pages/LoginPage.js
@@ -1,20 +1,49 @@
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
 
 function LoginPage() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [showpassword, setShowPassword] = useState("");
+  let navigate = useNavigate();
+
+  function togglePasswordVisiblity() {
+    setShowPassword(showpassword ? false : true);
+  }
+
+  function checkInfo() {
+    if (username === "rita123" && password === "66666") {
+      navigate("/datapage");
+    } else {
+      alert("oops, username or password is wrong");
+    }
+  }
+
   return (
     <div>
       <form>
         <label>
           Username:
-          <input type="text" name="name" placeholder="Email Address" />
+          <input
+            type="text"
+            placeholder="Email Address"
+            onChange={(event) => setUsername(event.target.value)}
+          />
         </label>
         <br />
         <label>
           Password:
-          <input type="password" name="psw" />
+          <input
+            type={showpassword ? "text" : "password"}
+            onChange={(event) => setPassword(event.target.value)}
+          />
         </label>
+        Show Password
+        <input type="checkbox" onClick={togglePasswordVisiblity} />
         <br />
-        <input type="submit" value="Login" />
+        <button type="submit" onClick={checkInfo}>
+          Login
+        </button>
         <button>
           <Link to="/signup">Sign Up</Link>
         </button>
